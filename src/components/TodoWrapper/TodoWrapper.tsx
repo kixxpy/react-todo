775ede4eaa969cf6f42e8a80f10ef91778a07db7
kirillEN <81442482+kixxpy@users.noreply.github.com>
2024-12-08 16:05:03 +0300

    В todowrapper редактирование header

--- src/components/TodoWrapper/TodoWrapper.tsx
+++ src/components/TodoWrapper/TodoWrapper.tsx
@@ -3,7 +3,9 @@ import styles from './TodoWrapper.module.css';
 export const TodoWrapper = () => {
 	return (
 		<div className={styles['todo-wrapper']}>
-			<div className={styles['header']}>header</div>
+			<div className={styles['header']}>
+				Мой список задач <span>количество задач 3</span>
+			</div>
 			<div className={styles['content']}>
 				loremhasdhkajhsdkhsakdhsadashdakjsdkahsdksahdasdasdasdasdasdasdasdashakdhasdhahdkhaskdadajkdhashdhadhsa
 			</div>
